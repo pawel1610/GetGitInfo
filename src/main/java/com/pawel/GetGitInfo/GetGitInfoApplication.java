66f1bd97f6aeed595debfd3ab79fd7252c2e08f0
pawel <pawelmaciaszek@gmail.com>
2019-05-17 11:24:37 +0200

    Initial commit

---
+++ src/main/java/com/pawel/GetGitInfo/GetGitInfoApplication.java
@@ -0,0 +1,13 @@
+package com.pawel.GetGitInfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetGitInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetGitInfoApplication.class, args);
+	}
+
+}
